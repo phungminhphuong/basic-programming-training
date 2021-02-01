87272a964e3239501ddb3ce3a0a88e363ef72e4b
dangquy <dangquy95php@gmail.com>
2021-02-01 23:27:33 +0700

    add

---
+++ example.js
@@ -0,0 +1,17 @@
+1. Nhập số n. Kiểm tra từ 0 tới n là số chẵn thì in ra.
+2. Nhập số n. Kiểm tra từ 0 tới n xem những số chia hết cho 5.
+3. Nhập số n. Kiểm tra những số không chia hết cho 2.
+4. Nhập n. Tính tổng các số từ 0 đến n.
+
+5. Cho 1 số n . Viết một chương trình để tính toán và in ra màn hình giai thừa của số đã nhập (n!)
+
+Ví dụ:
+
+Với n = 5, đầu ra được in ra màn hình là  "120"
+Vì 5! = 5 * 4 * 3 * 2 * 1 = 120
+Với n = 3, đầu ra được in ra màn hình là  "6"
+Vì 3! = 3 * 2 * 1 = 6
+Đầu vào: 1 số nguyên n nhập từ bàn phím
+Điều kiện tiền đề:
+1 ≤ n ≤ 20
+Đầu ra: giai thừa của số đã cho n
