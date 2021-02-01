524ccbc51ffde92bbaed34845d57956b27c781a3
dangquy <dangquy95php@gmail.com>
2021-02-01 21:31:38 +0700

    add bài tập

---
+++ example1.js
@@ -0,0 +1,26 @@
+1. Kiểm tra 1 số có phải là số chẵn hay không. Nếu chẵn in ra số n là số chẵn. Ngược lại số n là số lẻ.
+2. Kiểm tra 1 số có chia hết cho 3 hay không. Nếu chia hết cho 3 thì in ra số đó chia hết cho 3 và ngược lại.
+3. Người dùng sẽ nhập vào một số thứ 1 -> 7, trong đó số 1 là chủ nhật, số 2 là thứ 2, ... số 7 là thứ 7.
+Và để giải bài này thì bạn phải sử dụng nhiều lệnh if else liên tiếp nhau. 
+vd: Nhập số 1 in ra: Hôm nay là chủ nhật.
+4. Viết chương trình nhập vào 3 số và kiểm tra số nào là lớn nhất.
+5. Viết chương trình nhập vào 3 số và kiểm tra số nào là nhỏ nhất.
+6. VIết chương trình nhập vào 4 số kiểm tra số nhỏ nhất.
+7. Chương trình yêu cầu người dùng nhập điểm của ba môn toán, lý, hóa. Sau đó kiểm tra điểm trung bình:
+Nếu từ 8 điểm trở lên thì xếp loại giỏi
+Nếu từ 7 đến 6.9 điểm thì loại khá
+Nếu từ 5 đến 6.9 là trung bình
+Dưới 5 là yếu.
+8. Kiểm tra đăng nhập có chính xác không.
+Nhập username, và mật khẩu nếu đúng đăng nhập thành công ngược lại thất bại.
+Username mặt định là: test01, Mật khẩu mặc định là: mk01.
+
+9. Kiểm tra đăng nhập có chính xác không.
+Nhập username, và mật khẩu nếu đúng đăng nhập thành công ngược lại thất bại.
+Username mặt định là: test01, Mật khẩu mặc định là: mk01.
+Validate thông tin người dùng nhập:
+Nếu người dùng nhập username mà ít hơn 6 ký tự thì hiển thị cho người dùng biết là bạn không được nhập dưới 6 ký tự
+Nếu người dùng nhập mật khẩu mà ít hơn 4 ký tự thì hiển thị chi người dùng biết là bạn không được phép nhập dưới 4 ký tự.
+
+Nếu người dùng nhập username mà lớn hơn 10 ký tự thì hiển thị cho người dùng biết là bạn không được nhập lớn hơn 10 ký tự
+Nếu người dùng nhập mật khẩu mà lớn hơn 10 ký tự thì hiển thị chi người dùng biết là bạn không được phép nhập lớn hơn 10 ký tự.
